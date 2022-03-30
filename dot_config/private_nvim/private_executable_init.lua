b849e4b3a3071c5a29a9f2e37080be8d134e31fe
Fredy Rodriguez <fred.rdz@gmail.com>
2022-03-30 18:44:54 -0500

    updating nvim settings

--- dot_config/private_nvim/private_executable_init.lua
+++ dot_config/private_nvim/private_executable_init.lua
@@ -9,135 +9,69 @@ if fn.empty(fn.glob(install_path)) > 0 then
     execute 'packadd packer.nvim'
 end
 
-local indent = 2
-vim.opt.syntax = "on"
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.api.nvim_command("set nofoldenable")
-vim.opt.foldnestmax = 10
-vim.opt.foldlevel = 0
-vim.opt.foldcolumn = "0"
-vim.opt.shiftwidth = indent -- Size of an indent
-vim.opt.tabstop = indent -- Number of spaces tabs count for
-vim.opt.softtabstop = indent
-vim.opt.smartindent = true -- Insert indents automatically
-vim.opt.autoindent = true
-vim.opt.expandtab = true
-vim.opt.mouse = "" -- Disable mouse
-vim.opt.clipboard = "unnamedplus" -- Put those yanks in my os clipboards
-vim.opt.completeopt = "menuone,noselect" -- Completion options (for compe)
-vim.opt.hidden = true -- Enable modified buffers in background
-vim.opt.ignorecase = true -- Ignore case
-vim.opt.incsearch = true -- Make search behave like modern browsers
-vim.opt.inccommand = "split" -- Live preview for search and replace
-vim.opt.joinspaces = false -- No double spaces with join after a dot
-vim.opt.scrolloff = 10 -- Lines of context
-vim.opt.shiftround = true -- Round indent
-vim.opt.sidescrolloff = 8 -- Columns of context
-vim.opt.smartcase = true -- Don't ignore case with capitals
-vim.opt.splitbelow = true -- Put new windows below current
-vim.opt.splitright = true -- Put new windows right of current
-vim.opt.list = false -- Show some invisible characters (tabs...)
-vim.opt.number = true -- Print line number
-vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.wrap = true -- Enable line wrap
-vim.opt.cmdheight = 2 -- More space to display messages
-vim.opt.timeoutlen = 400 -- Don't wait more that 400ms for normal mode commands
-vim.opt.guicursor = {"n-v-c:block", "i-ci-ve:ver25", "r-cr:hor20", "o:hor50", "a:blinkwait700-blinkoff400-blinkon250", "sm:block-blinkwait175-blinkoff150-blinkon175"}
-vim.opt.cursorcolumn = false
-vim.opt.cursorline = true
-vim.opt.cursorlineopt = {"number", "line"}
-vim.opt.termguicolors = true -- True color support
-vim.opt.winblend = 8
-vim.opt.pumblend = 8
-vim.opt.wildignore = '*.swp,*.bak,*.pyc,*.class'
-vim.opt.wildmode = "list:longest" -- Command-line completion mode
-vim.opt.wildoptions = "pum"
-vim.opt.wildignorecase = true -- Ignore case command completion menu
-vim.opt.shada = {"!", "'1000", "<50", "s10", "h"} -- remember stuff across sessions
-vim.opt.undofile = true
-vim.opt.undodir = "/tmp/undodir"
-vim.opt.swapfile = false -- I have OCD file saving issues anyway
-vim.opt.backup = false
-vim.opt.writebackup = false
-vim.opt.lazyredraw = true
-vim.opt.ttyfast = true
-vim.opt.title = true
-vim.opt.updatetime = 300
-vim.opt.backspace = {"indent", "eol", "start"}
-vim.opt.fillchars:append({fold = ' '})
-vim.opt.fillchars:append({foldopen = '▾', foldsep = '│', foldclose = '▸'})
-vim.opt.fillchars:append({
-    horiz = '━',
-    horizup = '┻',
-    horizdown = '┳',
-    vert = '┃',
-    vertleft = '┨',
-    vertright = '┣',
-    verthoriz = '╋',
-})
-vim.opt.showbreak = "↪ "
-vim.opt.listchars = "tab:|.,trail:_,extends:>,precedes:<,nbsp:~,eol:¬"
-vim.opt.signcolumn = "yes"
-vim.opt.laststatus = 3
-
--------------------- set leader to space early --------------------
-vim.g.mapleader = " "
-
--------------------- set test strategy to vimux --------------------
-vim.g["test#strategy"] = "vimux"
-
--------------------- nvim.tree settings --------------------
-vim.g.nvim_tree_width = "10%"
-vim.g.nvim_tree_quit_on_open = 1
-vim.g.nvim_tree_respect_buf_cwd = 0
-
--------------------- vim-matchup settings --------------------
---vim.g.loaded_matchit = 1
-vim.g.matchup_matchparen_offscreen = {method = 'popup'}
-vim.g.matchup_matchparen_deferred = 1
-vim.g.matchup_matchparen_deferred_show_delay = 50
-vim.g.matchup_matchparen_deferred_hide_delay = 700
-vim.g.matchup_matchparen_hi_surround_always = 1
-vim.g.matchup_surround_enabled = 1
-
--------------------- hexokinase settings --------------------
-vim.g.Hexokinase_optInPatterns = {'full_hex', 'triple_hex', 'rgb', 'rgba', 'hsl', 'hsla', 'colour_names'}
-vim.g.Hexokinase_highlighters = {'virtual'}
-
 -------------------- load other lua configurations --------------------
-require("plugins")
-require("mappings")
-require("statusline")
-require("markdown")
-require("lspinstaller")
---require("go")
---require("lsp")
+require('nvim-settings')
+require('plugins')
+require('lspinstaller-settings')
+require('lspkind-settings')
+require('navigator-settings')
+require('mappings')
+require('markdown')
+require('statusline')
 
--------------------- testing options --------------------
-vim.cmd([[
-augroup FileTypeHighlight
-  autocmd!
-  au FileType git*,dap*,vista_kind,tagbar,fugitive set winhighlight=Normal:NormalFloat
-augroup END
-]])
+-------------------- functions --------------------
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+function OrgImports(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit)
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
 
--------------------- restore cursor position --------------------
+-------------------- autocmds --------------------
+-- in testing, highlighting?
+-- vim.cmd([[
+-- augroup FileTypeHighlight
+--   autocmd!
+--   au FileType git*,dap*,vista_kind,tagbar,fugitive set winhighlight=Normal:NormalFloat
+-- augroup END
+-- ]])
+
+-- restore cursor position
 vim.cmd(([[
   au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
 ]]))
 
--------------------- recompile automatically when editing plugins.lua --------------------
+-- recompile automatically when editing *.lua
 vim.cmd(([[
-  autocmd BufWritePost plugins.lua PackerCompile
+  autocmd BufWritePost *.lua PackerCompile
 ]]))
 
--------------------- efm-language-server (lua) settings --------------------
+-- auto-formatting settings
 -- lua auto-formatter
 -- run cmd: "lua vim.lsp.buf.formatting()"
 -- this will auto-format LUA files on save:
 vim.cmd(([[
   autocmd BufWritePre *.lua lua vim.lsp.buf.formatting_sync(nil, 100)
+]]))
+
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+vim.cmd(([[
+  autocmd BufWritePre *.go lua OrgImports(1000)
+  autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 100)
+]]))
+
+-- NvimTree auto close on last window
+vim.cmd(([[
+  autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
 ]]))
 
 -------------------- color scheme options  --------------------
