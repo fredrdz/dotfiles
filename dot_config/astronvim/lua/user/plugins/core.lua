c90e3b22dede1c3a0b7960e6a805838912c4d579
Fredy Rodriguez <fred.rdz@gmail.com>
2023-04-22 06:26:04 -0500

    astrovim updates

--- dot_config/astronvim/lua/user/plugins/core.lua
+++ dot_config/astronvim/lua/user/plugins/core.lua
@@ -33,35 +33,58 @@ return {
 	--     luasnip.filetype_extend("javascript", { "javascriptreact" })
 	--   end,
 	-- },
-	-- {
-	--   "windwp/nvim-autopairs",
-	--   config = function(plugin, opts)
-	--     require "plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-	--     -- add more custom autopairs configuration such as custom rules
-	--     local npairs = require "nvim-autopairs"
-	--     local Rule = require "nvim-autopairs.rule"
-	--     local cond = require "nvim-autopairs.conds"
-	--     npairs.add_rules(
-	--       {
-	--         Rule("$", "$", { "tex", "latex" })
-	--           -- don't add a pair if the next character is %
-	--           :with_pair(cond.not_after_regex "%%")
-	--           -- don't add a pair if  the previous character is xxx
-	--           :with_pair(
-	--             cond.not_before_regex("xxx", 3)
-	--           )
-	--           -- don't move right when repeat character
-	--           :with_move(cond.none())
-	--           -- don't delete if the next character is xx
-	--           :with_del(cond.not_after_regex "xx")
-	--           -- disable adding a newline when you press <cr>
-	--           :with_cr(cond.none()),
-	--       },
-	--       -- disable for .vim files, but it work for another filetypes
-	--       Rule("a", "a", "-vim")
-	--     )
-	--   end,
-	-- },
+	{
+		-- override nvim-autopairs plugin
+		"windwp/nvim-autopairs",
+		config = function(plugin, opts)
+			-- run default AstroNvim config
+			require("plugins.configs.nvim-autopairs")(plugin, opts)
+			-- require Rule function
+			local Rule = require("nvim-autopairs.rule")
+			local npairs = require("nvim-autopairs")
+			npairs.add_rules({
+				{
+					-- specify a list of rules to add
+					Rule(" ", " "):with_pair(function(options)
+						local pair = options.line:sub(options.col - 1, options.col)
+						return vim.tbl_contains({ "()", "[]", "{}", "{{}}" }, pair)
+					end),
+					Rule("( ", " )")
+						:with_pair(function()
+							return false
+						end)
+						:with_move(function(options)
+							return options.prev_char:match(".%)") ~= nil
+						end)
+						:use_key(")"),
+					Rule("{ ", " }")
+						:with_pair(function()
+							return false
+						end)
+						:with_move(function(options)
+							return options.prev_char:match(".%}") ~= nil
+						end)
+						:use_key("}"),
+					Rule("{{ ", " }}")
+						:with_pair(function()
+							return false
+						end)
+						:with_move(function(options)
+							return options.prev_char:match(".%}") ~= nil
+						end)
+						:use_key("}"),
+					Rule("[ ", " ]")
+						:with_pair(function()
+							return false
+						end)
+						:with_move(function(options)
+							return options.prev_char:match(".%]") ~= nil
+						end)
+						:use_key("]"),
+				},
+			})
+		end,
+	},
 	-- By adding to the which-key config and using our helper function you can add more which-key registered bindings
 	-- {
 	--   "folke/which-key.nvim",
