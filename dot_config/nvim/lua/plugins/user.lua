0a18b00e3458b8e1d01a5cd82a2ec5447b9ac82d
Fredy Rodriguez <fredy@thedesque.com>
2025-01-16 12:06:57 -0600

    feat(astronvim): add mini-jump, mini-pairs; disable nvim-autopairs; modify indent-blankline.nvim

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -168,37 +168,49 @@ return {
 		end,
 	},
 
+	{ "windwp/nvim-autopairs", optional = true, enabled = false },
 	{
-		"windwp/nvim-autopairs",
-		config = function(plugin, opts)
-			require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts) -- include the default astronvim config that calls the setup call
-			-- add more custom autopairs configuration such as custom rules
-			local npairs = require("nvim-autopairs")
-			local Rule = require("nvim-autopairs.rule")
-			local cond = require("nvim-autopairs.conds")
-			npairs.add_rules(
-				{
-					Rule("$", "$", { "tex", "latex" })
-						-- don't add a pair if the next character is %
-						:with_pair(cond.not_after_regex("%%"))
-						-- don't add a pair if  the previous character is xxx
-						:with_pair(
-							cond.not_before_regex("xxx", 3)
-						)
-						-- don't move right when repeat character
-						:with_move(cond.none())
-						-- don't delete if the next character is xx
-						:with_del(cond.not_after_regex("xx"))
-						-- disable adding a newline when you press <cr>
-						:with_cr(cond.none()),
+		"lukas-reineke/indent-blankline.nvim",
+		optional = true,
+		enabled = true,
+		config = function()
+			local highlight = {
+				"desque",
+			}
+
+			local hooks = require("ibl.hooks")
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "desque", { fg = "#16B2C3" })
+			end)
+
+			require("ibl").setup({
+				indent = { char = "" },
+				scope = {
+					highlight = highlight,
 				},
-				-- disable for .vim files, but it work for another filetypes
-				Rule("a", "a", "-vim")
-			)
+			})
 		end,
 	},
 
 	-- == Custom Plugins ==
+	{
+		"echasnovski/mini.jump",
+		event = "User",
+		version = false,
+		enabled = true,
+		config = function()
+			require("mini.jump").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		event = "InsertEnter",
+		version = false,
+		enabled = true,
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
 
 	{ "fredrdz/vim-hugo", ft = "gohtml" },
 	{ "tjdevries/templ.nvim", ft = "templ" },
