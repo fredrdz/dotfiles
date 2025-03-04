54d8a751b2640fcff25d0f9fac75a357afd905ad
Fredy Rodriguez <fredy@thedesque.com>
2025-03-03 23:59:49 -0600

    feat(astronvim): increase lsp format timeout

--- dot_config/nvim/lua/plugins/astrolsp.lua
+++ dot_config/nvim/lua/plugins/astrolsp.lua
@@ -51,7 +51,7 @@ return {
 					-- disable lua_ls formatting capability if you want to use StyLua to format your lua code
 					"lua_ls",
 				},
-				timeout_ms = 1000, -- default format timeout
+				timeout_ms = 5000, -- default format timeout
 				filter = function(client) -- fully override the default formatting function
 					if vim.bo.filetype == "astro" then
 						return client.name == "astro"
