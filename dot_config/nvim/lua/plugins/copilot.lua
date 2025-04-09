2571fc10d2287d16cfaceea510588691a457e296
Fredy Rodriguez <fredy@thedesque.com>
2025-04-08 23:52:01 -0500

    fix(astronvim): swap from copilot-cmp to copilot-lua-cmp to address completion issues

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,39 +1,39 @@
--- community plugin: copilot-cmp
+-- community plugin: copilot-lua-cmp
 -- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/completion/copilot-lua-cmp
 ---@type LazySpec
 return {
 	"AstroNvim/astrocommunity",
-	{ import = "astrocommunity.completion.copilot-cmp", enabled = true },
+	{ import = "astrocommunity.completion.copilot-lua-cmp", enabled = true },
 	-- modify copilot.lua: https://github.com/zbirenbaum/copilot.lua
-	{
-		"copilot.lua",
-		event = "InsertEnter",
-		enabled = true,
-		opts = {
-			panel = {
-				enabled = false,
-				auto_refresh = true,
-				---@class copilot.panel
-				---@field position? "top"|"bottom"|"left"|"right"|"horizontal"|"vertical"
-				layout = {
-					position = "bottom",
-					ratio = 0.20, -- 20% of the screen
-				},
-			},
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				hide_during_completion = true,
-				debounce = 500,
-				keymap = {
-					accept = "<CM-a>", -- [a]ccept
-					accept_line = "<CM-l>", -- [l]ine
-					accept_word = "<CM-w>", -- [w]ord
-					prev = "<CM-e>", -- pr[e]v
-					next = "<CM-n>", -- [n]ext
-					dismiss = "<CM-m>", -- dis[m]iss
-				},
-			},
-		},
-	},
+	-- {
+	-- 	"copilot.lua",
+	-- 	event = "InsertEnter",
+	-- 	enabled = true,
+	-- 	opts = {
+	-- 		panel = {
+	-- 			enabled = false,
+	-- 			auto_refresh = true,
+	-- 			---@class copilot.panel
+	-- 			---@field position? "top"|"bottom"|"left"|"right"|"horizontal"|"vertical"
+	-- 			layout = {
+	-- 				position = "bottom",
+	-- 				ratio = 0.20, -- 20% of the screen
+	-- 			},
+	-- 		},
+	-- 		suggestion = {
+	-- 			enabled = true,
+	-- 			auto_trigger = true,
+	-- 			hide_during_completion = true,
+	-- 			debounce = 500,
+	-- 			keymap = {
+	-- 				accept = "<CM-a>", -- [a]ccept
+	-- 				accept_line = "<CM-l>", -- [l]ine
+	-- 				accept_word = "<CM-w>", -- [w]ord
+	-- 				prev = "<CM-e>", -- pr[e]v
+	-- 				next = "<CM-n>", -- [n]ext
+	-- 				dismiss = "<CM-m>", -- dis[m]iss
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 }
